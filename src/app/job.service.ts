14b8785098940d3da9f01b8b6f7be7e61b54485f
kevin-ziteng <kevin.ztwang@gmail.com>
2018-06-28 23:11:03 -0400

    update job service

--- src/app/job.service.ts
+++ src/app/job.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Job } from './job';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  constructor() { }
+  private handleError(error: HttpErrorResponse){
+    if (error.error instanceof ErrorEvent){
+      console.error('An error occurred: ', error.error.message);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError('Something bad happened!');
+  }
+
+  getJobList(): Observable<Job[]>{
+    return this.http.get<Job[]>('http://andybao.net/apis/magic.php?api_key=123&job=a').pipe(
+      catchError(this.handleError)
+    );  
+  }
+
+  getJob(id: string): Observable<Job>{
+      return this.http.get<Job>(`http://andybao.net/apis/magic.php?api_key=123&id=${id}`).pipe(
+        catchError(this.handleError)
+      ); 
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
 }
