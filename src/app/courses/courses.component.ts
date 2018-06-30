19f5a20b3ec1c0f206db7df838ebd5a3301939cf
andybao <wenyu.bao@gmail.com>
2018-06-29 21:52:35 -0400

    andy: course feature

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CourseService} from "../course.service";
+import {Course} from "../course";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Planet} from "../../../../../7_week/Planets/src/app/planet";
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+  
+  courses: Course[];
+  course: Course;
 
-  constructor() { }
+  constructor(
+      private route: ActivatedRoute,
+      private courseService: CourseService
+  ) { }
+  
+  getCourses() {
+
+      this.courseService.getCourses().subscribe(data => this.courses = data);
+  }
+
+  onSelect(c: Course): void {
+      //this.courseService.getCourse(c.code).subscribe(data => data[0]);
+      this.courseService.getCourse(c.code).subscribe(data => this.course = data);
+      //this.getCourse(c.code);
+      //this.course = this.courses[0];
+    }
+
+  onSubmit() {
+      alert('Success');
+  }
 
   ngOnInit() {
+      this.getCourses();
   }
 
 }
